eb906c3b5d08b7b6fb7c4aa854bf487148a6b802
Greg Slowski <gslowski@ualberta.ca>
2021-11-17 20:04:42 -0700

    VET000-62 Added password encoder/decoder

---
+++ src/main/java/api/admin/PasswordEncoder.java
@@ -0,0 +1,17 @@
+package api.admin;
+
+import java.util.Base64;
+
+public class PasswordEncoder {
+
+	public static String encode(String password) {
+		return Base64.getEncoder().encodeToString(password.getBytes());
+	}
+	
+	public static String decode(String encodedPassword) {
+		byte[] decodedBytes = Base64.getDecoder().decode(encodedPassword);
+		String decodedString = new String(decodedBytes);
+		return decodedString;
+	}
+	
+}
