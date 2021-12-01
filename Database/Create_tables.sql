0844bb326078ca036f11a0b1f0f3ac58b30fbc0e
Drew Burritt <drew.burritt@ucalgary.ca>
2021-12-01 12:53:44 -0700

    database files

---
+++ Database/Create_tables.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS PRESCRIPTION;
+CREATE TABLE PRESCRIPTION (
+	PrescriptionId			integer	not null,
+	TreatmentId				integer not null,
+	AnimalId				integer not null,
+    Prescription			varchar(50) not null,
+    Notes					mediumtext,
+	primary key (PrescriptionId),
+    foreign key (TreatmentId) references TREATMENT(TreatmentId),
+	foreign key (AnimalId) references Animal(AnimalId)
+);
+
+DROP TABLE IF EXISTS COMMENTS;
+CREATE TABLE COMMENTS (
+	CommentId			integer	not null,
+	UserId				integer not null,
+	AnimalId				integer not null,
+    CommentDate				DATETIME not null,
+    CommentText					mediumtext not null,
+	primary key (PrescriptionId),
+    foreign key (UserId) references USER(UserId),
+	foreign key (AnimalId) references Animal(AnimalId)
+);
+
+DROP TABLE IF EXISTS ANIMAL_STATUS;
+CREATE TABLE ANIMAL_STATUS (
+	AnimalId			integer	not null,
+	AnimalStatus		VARCHAR(100) not null,
+    StatusDate				DATETIME not null,
+	primary key (AnimalId),
+	foreign key (AnimalId) references Animal(AnimalId)
+);
