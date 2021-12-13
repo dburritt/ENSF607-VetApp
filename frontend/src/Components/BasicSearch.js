0495533de0d5c1017c523835cfcd6570a1f09477
Thomas <thomas.scott@ucalgary.ca>
2021-12-13 02:50:16 -0700

    Added advanced search filtering (incomplete)

--- frontend/src/Components/BasicSearch.js
+++ frontend/src/Components/BasicSearch.js
@@ -3,28 +3,19 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { css } from "@emotion/react";
 import 'bulma/css/bulma.css';
-import AdvancedSearch from '../Components/AdvancedSearch';
-
-const AdvancedSearchPopup = props => {
-    return (
-        <div className="popup-box">
-          <div className="box">
-            <button className="button" onClick={props.handleClose}>Close</button>
-            <AdvancedSearch/>
-          </div>
-        </div>
-      );
-}
 
 const BasicSearchView = ({ user, pageDispatch, animalSelectionDispatch }) => {
     useEffect(() => {
         fetchAllAnimals();
+        fetchSpecies();
     }, []);
     const [results, setResults] = useState([]);
 
     const [selected, setSelected] = useState([]);
     const [search, setSearch] = useState("");
     const [advancedSearchOpen, setAdvancedSearchOpen] = useState(false);
+    const [searchSpeciesFilter, setSearchSpeciesFilter] = useState([]);
+    const [searchFilterOptions, setSearchFilterOptions] = useState([]);
 
     const toggleAdvancedSearch = () => {
         setAdvancedSearchOpen(!advancedSearchOpen);
@@ -63,6 +54,20 @@ const BasicSearchView = ({ user, pageDispatch, animalSelectionDispatch }) => {
                 setResults([]);
             });
     };
+
+    const fetchSpecies = () => {
+        axios.get('http://localhost:8001/api/animals?subspecies=0')
+            
+            .then((res) => {
+                setSearchSpeciesFilter(res.data.animals);
+                console.log(res.data.animals);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+      };
+
+
     const selectHandler = (animal) => {
         setSelected(animal);
         // console.log(selected);
@@ -112,6 +117,41 @@ const BasicSearchView = ({ user, pageDispatch, animalSelectionDispatch }) => {
             nextPage: "animalProfile"
         });
     };
+
+    const AdvancedSearchPopup = props => {
+        return (
+            <div className="popup-box">
+              <div className="box">
+                <button className="button" onClick={props.handleClose}>Close</button>
+                <div>
+                <header>
+                    <span>Advanced Search</span>
+                </header>
+    
+                <div class="box">
+                    <div class="select is-primary">
+                        <select>
+                            <option>Filter By</option>
+                            <option onClick={() => setSearchFilterOptions(searchSpeciesFilter.map(sp => (
+                                                            <option onClick={() => setResults(results.filter(r => 
+                                                            r.species === sp.species))}>{sp.species}</option>)))}>Species</option>
+                            <option>Subspecies</option>
+                            <option>Breed</option>
+                            <option>Sex</option>
+                        </select>
+                    </div>
+                    <div class="select is-primary">
+                        <select>
+                            <option>Options</option>
+                            {searchFilterOptions}
+                        </select>
+                    </div>
+                </div>
+            </div>
+              </div>
+            </div>
+          );
+    }
 
     return (
         <div className="column is-four-fifths">
